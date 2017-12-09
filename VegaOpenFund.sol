e0192bd54b6e604d850472a5c20cde03da576a16
HAL9000 <georgek00012@gmail.com>
2017-12-08 19:28:05 -0500

    Subscription and Redemption methods created

--- VegaOpenFund.sol
+++ VegaOpenFund.sol
@@ -1,76 +1,81 @@
-pragma solidity ^0.4.18;
-contract SafeMath {
-  function safeMul(uint a, uint b) internal returns (uint) {
-    uint c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
+pragma solidity ^0.4.15;
 
-  function safeSub(uint a, uint b) internal returns (uint) {
-    assert(b <= a);
-    return a - b;
-  }
 
-  function safeAdd(uint a, uint b) internal returns (uint) {
-    uint c = a + b;
-    assert(c>=a && c>=b);
-    return c;
-  }
+contract Fund {
+    
+    mapping(address => uint) public VFT;
+    mapping(address => uint) public VNT; // just for testing.
+    
 
-  function assert(bool assertion) internal {
-    if (!assertion) throw;
-  }
-    function multiplyPercentage(uint _value, uint _numerator, uint _denominator) public constant returns (uint) {
-        uint value = (_value * _numerator) / _denominator;
-        return value;
+    uint public subscriptionFee = 2;
+    uint public redemptionFee = 2;
+    uint public NAV = 1;
+    uint public VNTToEther = 1;
+    uint public totalFeesAmountInEther;
+    uint public totalFeesInVNT;
+    uint public vftTotalSupply;
+    address public valut;
+    
+    
+    function Fund() {
+        VNT[msg.sender] = 10000000000000000000000000;
     }
-}
-
-
-contract VegaOpenFund is SafeMath{
-
-    mapping (address => uint256) public balances;
-    uint public totalSupply;
-    uint public etherHoldings;
-
-    event LogSubscription(address sender, uint amount);
-    event LogRedemption(address receiver, uint amount);
-    event LogTransfer(address sender, address to, uint amount);
-
-    // Get the price per token in the Vega Open Fund.
-    function getCurrentTokenPriceInEth() public constant returns (uint) {
-        uint price = etherHoldings / totalSupply;
-        return price;
-    }
-
-    // Subscribe to tokens in the Vega Open Fund.
-    function subscribe() payable returns(bool success) {
-        uint amount = msg.value / getCurrentTokenPriceInEth();
-        balances[msg.sender] +=amount;
-        totalSupply += amount;
-        etherHoldings += msg.value;
-        LogSubscription(msg.sender, amount);
+    
+    /* SUBSCRIPTIONS AND REDEMPTIONS */
+    
+    /* subscribe to VFT using VNT */
+    
+    function subscribe() payable returns (bool success) {
+        uint amount = msg.value / NAV;
+        paySubscriptionFee(amount);
+        VFT[msg.sender] += amount;
+        vftTotalSupply += amount;
         return true;
     }
-
-    // Redeem fund interests in the Vega Open Fund.
-    function redeem(uint tokens) returns(bool success) {
-        if(balances[msg.sender] < tokens) throw;
-        balances[msg.sender] -= tokens;
-        uint amount = safeMul(tokens, getCurrentTokenPriceInEth());
+    
+    /* redeem VFT using VNT */
+    
+    function redeem(uint _VFT) returns (bool success) {
+        require(VFT[msg.sender] >= _VFT);
+        uint amount = VFT[msg.sender] * NAV;
+        payRedemptionFee(amount);
+        VFT[msg.sender] -= _VFT;
+        vftTotalSupply -= _VFT;
         msg.sender.transfer(amount);
-        totalSupply -= tokens;
-        etherHoldings -= amount;
-        LogRedemption(msg.sender, tokens);
         return true;
     }
-
-    // Transfer VFT from one account to another.
-    function transfer(address to, uint tokens) returns(bool success) {
-        if(balances[msg.sender] < tokens) throw;
-        balances[msg.sender] -= tokens;
-        to.transfer(tokens);
-        LogTransfer(msg.sender, to, tokens);
-        return true;
+    
+    /* pay subscription fee in VNT */
+    
+    function paySubscriptionFee(uint _amount) internal {
+        uint num = (_amount * subscriptionFee) / 100;
+        uint num2 = num / VNTToEther;
+        require(VNT[msg.sender] >= num2);
+        VNT[msg.sender] -= num2;
+        totalFeesAmountInEther += num;
+        totalFeesInVNT += num2;
     }
+    
+    /* pay redemption fee in VNT */
+    
+    function payRedemptionFee(uint _amount) internal {
+        uint num = (_amount * redemptionFee) / 100;
+        uint num2 = num / VNTToEther;
+        require(VNT[msg.sender] >= num2);
+        VNT[msg.sender] -= num2;
+        totalFeesAmountInEther += num;
+        totalFeesInVNT += num2;
+    }
+    
+    /* CUSTODY MANAGEMENT */
+    
+    /* deposit funds from Fund to Network valut */
+    
+    function depositToValut(uint _wei) returns (bool success);
+    
+    /* withdrawal funds from Network valut to Fund */
+    
+    function withdrawalFromValut(uint _wei) returns (bool success);
+    
 }
+
